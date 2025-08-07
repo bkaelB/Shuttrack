4fd3f8a00c5d108f808f7309ab8be0d10b59b69a
bkaelB <116369856+bkaelB@users.noreply.github.com>
2025-08-07 13:01:42 +0800

    Add Players Button

---
+++ components/AddQueueButton.jsx
@@ -0,0 +1,9 @@
+const AddQueueButton = ({player, onAdd}) => {
+  return (
+    <button className=" px-3 bg-blue-500 text-white rounded rounded-xl hover:bg-blue-600 cursor-pointer" onClick={()=> onAdd(player)}>
+        Add Queue
+    </button>
+  )
+}
+
+export default AddQueueButton
--- pages/MatchPage.jsx
+++ pages/MatchPage.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import AddQueueButton from "../components/AddQueueButton";
+
 import axios from "axios"
 
 const MatchPage = () => {
@@ -16,10 +18,20 @@ const MatchPage = () => {
 
   }, []);
 
+
+  const [queuedPlayers, setQueuedPlayers] = useState([]);
+
+  const handleAddToQueue = (player) => {
+    if (queuedPlayers.length >= 4) return;
+    if (queuedPlayers.find(p => p.id === player.id)) return;
+
+    setQueuedPlayers(prev => [...prev, player]);
+  }
+
   return (
     <>
-      <div className="p-4 grid grid-cols-4 gap-4">
-        <div className="col-span-1 space-y-4">
+      <div className="p-4 flex gap-4">
+        <div className=" space-y-4 w-1/4">
           <h2 className="text-xl font-bold mb-2">Players</h2>
           {isLoading ? (
             <p>Loading players...</p>
@@ -27,19 +39,46 @@ const MatchPage = () => {
             players.map(player => (
               <div className="bg-white shadow p-4 rounded-lg" key={player.id}>
                 <p className="font-semibold">{player.name}</p>
-                <p>Level: {player.level}</p>
-                <p>Games Played: {player.games_played}</p>
+                <div className="flex space-x-2">
+                  <p>Level: {player.level}</p>
+                  <p>Games Played: {player.games_played}</p>
+                  <AddQueueButton player={player} onAdd={handleAddToQueue} />
+                </div>
               </div>
             ))
           )}
         </div>
 
-        <div className="col-span-3">
-          <h2 className="text-xl font-bold mb-4"> aray koh!</h2>
+        <div className="w-3/4 p-4 space-y-4">
+          <div className="grid grid-cols-5 gap-4 items-center w-full">
+            {/* Team A - Player 1 */}
+            {queuedPlayers.slice(0, 2).map(player => (
+              <div className="bg-white shadow p-4 rounded-lg text-center w-full" key={player.id}>
+                <div className="font-semibold">{player.name}</div>
+                <div className="flex flex-row justify-center space-x-2 text-sm text-gray-600">
+                  <p>Level: {player.level}</p>
+                  <p>Games Played: {player.games_played}</p>
+                </div>
+              </div>
+            ))}
+
+            {/* VS Divider */}
+            {queuedPlayers.length === 4 && (<div className="text-center font-bold text-2xl p-0">VS</div> )}
+            
+
+            {/* Team B - Player 1 */}
+              {queuedPlayers.slice(2, 4).map(player => (
+                <div key={player.id} className="bg-white shadow p-4 rounded-lg text-center w-full">
+                  <p className="font-semibold">{player.name}</p>
+                  <div className="flex flex-row justify-center space-x-2 text-sm text-gray-600">
+                    <p>Level: {player.level}</p>
+                    <p>Games Played: {player.games_played}</p>
+                  </div>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
-
-
     </>
   );
 };
